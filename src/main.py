7736de82d430e64ad93eeda2b4552630a6708356
tim frazer <tim@littlebigthings.co>
2021-03-16 08:25:27 -0400

    fixes

--- src/main.py
+++ src/main.py
@@ -1,30 +1,31 @@
+from enum import Enum
+from typing import Optional, Tuple
+
 from fastapi import FastAPI
 from pydantic import BaseModel
-from pydantic.dataclasses import dataclass
-from typing import Tuple, Optional
-from enum import Enum
-
 
 app = FastAPI()
 
+
 class Battery(str, Enum):
- standard = 'standard'
- long_range = 'long range'
+    standard = "standard"
+    long_range = "long range"
 
 
 class TeslaCarModels(str, Enum):
     pass
 
-class Tesla(BaseModel):
- style: str
- battery_pack: Tuple[Battery, ...]
- insane_mode: Optional[bool] = None
 
+class Tesla(BaseModel):
+    style: str
+    battery_pack: Tuple[Battery, ...]
+    insane_mode: Optional[bool] = None
 
 
 def make_order(tesla: Tesla):
     # Business logic for creating a Tesla
-    return {'tesla': tesla}
+    return {"tesla": tesla}
+
 
 @app.post("/order/tesla")
 async def order_tesla(tesla: Tesla):
